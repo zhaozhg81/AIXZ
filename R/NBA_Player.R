8f226b7a64f3b59a51398fe307e05ef45ef08cf4
Zhigen Zhao <zhigen.zhao@gmail.com>
2019-10-21 21:30:56 -0400

    update files

--- R/NBA_Player.R
+++ R/NBA_Player.R
@@ -1,4 +1,4 @@
-NBA <- read.table("../data/NBA_Payer_Salary.csv",header=T, sep=",")
+NBA <- read.table("data/NBA_Payer_Salary.csv",header=T, sep=",")
 
 NBA$X2019 <- round(NBA$X2019/1000000,digits=2)
 NBA$X2020 <- round(NBA$X2020/1000000,digits=2)
