a5ea6687e58db3c54cf500017a958d5c665bafdb
Zhigen Zhao <zhaozhg@guoliang.guoliang>
2020-09-21 13:46:18 -0400

    update faithful_kde

--- R/faithful_kde.R
+++ R/faithful_kde.R
@@ -126,7 +126,10 @@ plot( xaxis, emp.cdf, main="Faithful", xlab="X", ylab="cdf", cex.main=2, cex.lab
 
 ## Gaussian Mixture Model
 em.esti <- EM( X, K=2, pi.ini= c(0.5,0.5), mu.ini=c(-1,1), sigma.ini=c(1,1), verbose=TRUE )
+plot( xaxis, em.esti$pi.esti[1] * pnorm( xaxis, em.esti$mu.esti[1], em.esti$sigma.esti[1]) + em.esti$pi.esti[2] * pnorm(xaxis, em.esti$mu.esti[2], em.esti$sigma.esti[2]), 'l', col='red' )
+points( xaxis, emp.cdf, col='green', 'l') 
 
 
+## KS test for mixture of normal
 ks.res = ks_test_GMM( X, 2, em.esti$pi.esti, em.esti$mu.esti, em.esti$sigma.esti )
   
