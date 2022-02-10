307eefa394eb9a9f2ccd3b674ac9fdcf43d2e5d5
Zhigen Zhao <zhigen.zhao@gmail.com>
2022-02-10 13:59:28 -0500

    apdate files

--- R/pneumoconiosis.R
+++ R/pneumoconiosis.R
@@ -6,6 +6,42 @@ attach(pneumo.df)
 
 lodds <- function(x,y){log((x+.5)/(y+.5))}
 
+multi.model <- vglm( cbind(normal,mild,severe)~ log(time), data=pneumo.df, fam=multinomial(parallel=TRUE~time-1) )
+
+plot( time, lodds(normal,severe))
+plot( time, lodds(mild,severe) )
+
+
+
+plot( log(time), lodds(normal,severe))
+plot( log(time), lodds(mild,severe) )
+
+
+multi.model <- vglm( cbind(normal,mild,severe)~ log(time), data=pneumo.df, fam=multinomial(parallel=TRUE~time-1) )
+
+## Fitted
+base.fit.value <- fitted( multi.model )
+
+pnewmo.base.normal <- base.fit.value[,1]
+pnewmo.base.mild <- base.fit.value[,2]
+pnewmo.base.severe <- base.fit.value[,3]
+cbind(pnewmo.base.normal,pnewmo.base.mild,pnewmo.base.severe)
+
+plot( log(time), lodds( normal, severe), xlab="", ylab="Base Logit")
+points( log(time), log( pnewmo.base.normal/pnewmo.base.severe), 'l', col='red' )
+
+## Check the graph
+
+r1.base <- pnewmo.base.normal
+r2.base <- pnewmo.base.normal+pnewmo.base.mild
+
+plot(log(time), lodds(normal, mild+severe), xlab="", ylab="1st CUM.LOGIT")
+points( log(time), log(r1.base/(1-r1.base)), 'l', col='green')
+
+plot(log(time), lodds(mild+normal, severe), xlab="Log Exposure", ylab="2nd Cum.Logit")
+points( log(time), log(r2.base/(1-r2.base)), 'l', col='green')
+
+
 clm.fit <- vglm( cbind( normal, mild, severe)~log(time), family=cumulative(parallel=TRUE~log(time)-1 ), data=pneumo.df)
 summary( clm.fit)
 fit.value <- fitted( clm.fit )
@@ -24,16 +60,23 @@ r1 <- pnewmo.po.normal
 r2 <- pnewmo.po.normal+pnewmo.po.mild
 
 
+
 ## Save the file
 ## postscript("plot_pneu_a.eps",horizontal=FALSE)
 plot(log(time), lodds(normal, mild+severe), xlab="", ylab="1st CUM.LOGIT")
 title("Cumulative logit")
 points( log(time), log(r1/(1-r1)), 'l', col='red')
+points( log(time), log(r1.base/(1-r1.base)), 'l', col='green')
+legend('bottomleft', c('data','clm','blm'),lty=c(1,1,1),col=c('black','red','green'),cex=2 )
+       
 ## dev.off()
 ## postscript("plot_pneu_b.eps",horizontal=FALSE)
 plot(log(time), lodds(mild+normal, severe), xlab="Log Exposure", ylab="2nd Cum.Logit")
 title("Cumulative logit")
 points( log(time), log(r2/(1-r2)), 'l',col='red')
+points( log(time), log(r2.base/(1-r2.base)), 'l', col='green')
+legend('bottomleft', c('data','clm','blm'),lty=c(1,1,1),col=c('black','red','green'),cex=2 )
+
 ## dev.off()
 ## postscript("plot_pneu_c.eps",horizontal=FALSE)
 plot( log(time), pnewmo.po.normal, 'l',col='green' ,ylim=c(0,1) )
