255ebf3d5e0c6afe70e4a2c557e7e5bb23385b9b
Zhigen Zhao <zhigen.zhao@gmail.com>
2022-04-07 13:45:14 -0400

    update

--- R/simpson.R
+++ R/simpson.R
@@ -23,6 +23,25 @@ plot( x1, y1, col='red', xlim=c( min(x1,x2)-1, max(x1,x2)+1), ylim=c( min(y1,y2)
 points(x2,y2, col='green')
 
 
+## Kidney stone data
+stone = NULL
+stone$Z=c(0,0,0,0,1,1,1,1) ## 0 means small stone, 1 means large stone
+stone$X=c(0,0,1,1,0,0,1,1) ## 0 means treatment A, 1 means treatment B
+stone$Y=c(81,6,234,36,192,71,55,25) ## Number of recovery vs non-recovery
+
+rates <- NULL
+rates$A <- c( stone$Y[1]/(stone$Y[1]+stone$Y[2]), stone$Y[5]/(stone$Y[5]+stone$Y[6]))
+rates$B <- c( stone$Y[3]/(stone$Y[3]+stone$Y[4]), stone$Y[7]/(stone$Y[7]+stone$Y[8]))
+rates$overall <- c( sum( stone$Y[c(1,5)] )/sum(stone$Y[c(1,2,5,6)]) , sum( stone$Y[c(3,7)] )/sum(stone$Y[c(3,4,7,8)]) )
+
+## Adjusted estimator
+P.Z1 <- sum( stone$Z*stone$Y)/sum(stone$Y)
+P.Z0 <- sum( (1-stone$Z)*stone$Y)/sum(stone$Y)
+
+E.Y0 <- stone$Y[1]/(stone$Y[1]+stone$Y[2]) * P.Z0 + stone$Y[5]/(stone$Y[5]+stone$Y[6]) * P.Z1
+E.Y1 <- stone$Y[3]/(stone$Y[3]+stone$Y[4]) * P.Z0 + stone$Y[7]/(stone$Y[7]+stone$Y[8]) * P.Z1
+
+
 ## Berkeley admission data
 berkeley <- read.csv("data/berkeley.csv")
 
@@ -42,3 +61,9 @@ applicant.ratio <- apply(Accepted+Rejected,1,sum)/(total.female+total.male)
 
 sum( cond.accept.rate[,1] * applicant.ratio - cond.accept.rate[,2] * applicant.ratio )
 
+## ATT
+P.dept.cond.Female <- (Accepted[,1]+Rejected[,1])/apply( Accepted+Rejected,2,sum)[1]
+P.cond.male.dept <- Accepted[,2]/(Accepted[,2]+Rejected[,2])
+
+E.Y0 <- mean( P.cond.male.dept )
+E.Y1 <- sum( Accepted[,2])/(sum(Accepted[,2])+sum(Rejected[,2]))
