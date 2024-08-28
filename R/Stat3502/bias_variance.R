dc2b01f23a71dcce471f4af88e727b67249a93ca
Zhigen Zhao <zhigen.zhao@gmail.com>
2024-08-28 11:42:06 -0400

    Update bias_variance.R

--- R/Stat3502/bias_variance.R
+++ R/Stat3502/bias_variance.R
@@ -1,5 +1,6 @@
 ## The common population is N(mu, 1)
-mu = 1
+mu = 1.75
+sigma=0.2
 
 ## Bias for a fixed sample size
 n = 100
@@ -10,7 +11,7 @@ sample_variance = rep(0, num_rep_sampling)
 
 for( i in 1:num_rep_sampling)
 {
-  X = rnorm(n, mu, 1)
+  X = rnorm(n, mu, sigma)
   sample_mean_full[i] = mean(X)
   sample_mean_partial[i] = mean( X[1:10]) 
   sample_variance[i] = var( X )
@@ -29,7 +30,7 @@ ns = 100*c(1:30)
 sample_mean_all = array(0, c(length(ns), num_rep_sampling) )
 for( i in 1:30)
   {
-    X = array( rnorm( ns[i]*num_rep_sampling, mu, 1), c( ns[i], num_rep_sampling) )
+    X = array( rnorm( ns[i]*num_rep_sampling, mu, sigma), c( ns[i], num_rep_sampling) )
     sample_mean_all[i,] = apply(X, 2, mean)
     print(i)
 }
