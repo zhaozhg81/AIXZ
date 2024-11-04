ef4a583c947e2ec2ccf6c0e996b01c015b1a0af4
Zhigen Zhao <zhaozhg@hinoki.hinoki>
2024-11-04 12:39:40 -0500

    update

--- R/Stat3502/cement.r
+++ R/Stat3502/cement.r
@@ -24,4 +24,9 @@ models <- lapply(formulas, lm, data=mydata)
 cor( mydata )
 pairs(~x1+x2+x3+x4, data=mydata)
 
-ols_mallows_cp( models[1], models[15])
+model1= lm(formulas[1]$`y~x1`, data=mydata) 
+model15= lm( formulas[15]$`y~x1+x2+x3+x4`, data=mydata)
+model5 = lm( formulas[5]$`y~x1+x2`, data=mydata)
+
+ols_mallows_cp( model1, model15) 
+ols_mallows_cp( model5, model15) 
