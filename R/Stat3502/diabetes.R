11f6249cc73b7211b33a4f1b5b15f2360d68d6f2
Zhigen Zhao <zhaozhg@hinoki.hinoki>
2024-11-08 12:21:18 -0500

    update

--- R/Stat3502/diabetes.R
+++ R/Stat3502/diabetes.R
@@ -1,9 +1,23 @@
 library(lars)
+library(glmnet)
+
 
 data(diabetes)
+
 par(mfrow=c(1,2))
 attach(diabetes)
+
 object <- lars(x,y)
+
 plot(object)
+
 object2 <- lars(x,y,type="lar")
 plot(object2)
+
+## K-fold cross-validation
+cv_model = cv.glmnet( x, y, alpha=1, nfolds=10 )
+best_lambda = cv_model$lambda.min
+
+best_model = glmnet(x,y, alpha=1, lambda=best_lambda)
+
+coef( best_model )
