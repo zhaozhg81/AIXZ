e1dcde382ba15c86c86f65bd28438df9d8d29252
Zhigen Zhao <zhaozhg@hinoki.hinoki>
2024-11-15 12:34:53 -0500

    update

---
+++ R/Stat3502/mtcars.r
@@ -0,0 +1,39 @@
+library(pls)
+
+#make this example reproducible
+set.seed(1)
+
+head(mtcars)
+?mtcars
+
+round( cor(mtcars[,2:7]), digits=2 )
+
+lm.model <- lm(hp~mpg+disp+drat+wt+qsec, data=mtcars)
+summary(model)
+
+
+pca.fit = prcomp(~mpg+disp+drat+wt+qsec,data=mtcars, scale=T)
+
+#fit PCR model
+model <- pcr(hp~mpg+disp+drat+wt+qsec, data=mtcars, scale=TRUE, validation="CV")
+
+#view summary of model fitting
+summary(model)
+
+
+#define training and testing sets
+train <- mtcars[1:25, c("hp", "mpg", "disp", "drat", "wt", "qsec")]
+y_test <- mtcars[26:nrow(mtcars), c("hp")]
+test <- mtcars[26:nrow(mtcars), c("mpg", "disp", "drat", "wt", "qsec")]
+
+## Use linear model
+lm.model = lm(hp~mpg+disp+drat+wt+qsec, data = train)
+lm_pred = predict( lm.model, test )
+sqrt( mean((lm_pred-y_test)^2))
+
+#use model to make predictions on a test set
+model <- pcr(hp~mpg+disp+drat+wt+qsec, data = train, scale =TRUE, validation = "CV")
+pcr_pred <- predict(model, test, ncomp = 2)
+
+#calculate RMSE
+sqrt(mean((pcr_pred - y_test)^2))
--- R/Stat8112_8113/score_phy_stat.R
+++ R/Stat8112_8113/score_phy_stat.R
@@ -1,6 +1,14 @@
+set.seed(2)
+English = rnorm(100, 75, 2)
+
 
 dat = read.table("./data/score_phy_stat.dat",head=T)
-plot(dat$Phys, dat$Stat)
+dat = read.table("./data/3502/score_phy_stat_english.dat",head=T)
+
+plot( dat$Phys, dat$English, xlim=c(0,100), ylim=c(0,100))
+
+
+plot(dat$Phys, dat$Stat, xlim=c(0,100), ylim=c(0,100))
 
 pc = princomp(~Stat+Phys,dat)
 
